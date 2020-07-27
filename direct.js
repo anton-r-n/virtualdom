1bbc0ce271a21f77257d64982cd7de2bb60c65dd
Anton Rogozhkin <anton.r7n@gmail.com>
2020-07-27 01:24:58 -0700

    Simplify widgets

--- direct.js
+++ direct.js
@@ -1,5 +1,3 @@
-// prettier-ignore
-
 'use strict';
 
 (function(w) {
@@ -11,10 +9,11 @@
 
   /* Render elt recursively according to spec */
   function direct(elt, spec, fn) {
+    elt = elt || 1;
     if (typeof fn === 'function') callback(fn);
-    if (spec == null) return text(elt, '');
-    if (spec.widget) return widget(elt, spec, '' + spec.widget);
-    if (spec.name) return element(elt, spec);
+    if (!spec) return text(elt, '');
+    if (spec.widget) return widget(elt, spec);
+    if (spec.name) return update(elt, spec, elt.__spec || 1);
     return text(elt, spec);
   }
 
@@ -23,27 +22,30 @@
     w.setTimeout(function() {d.body.clientWidth, fn()});
   }
 
-  /* Handle a case when spec is widget */
-  function widget(elt, spec, wn) {
-    var fn = wn.substr(0, 6) === 'Widget' ? $[wn] : err;
-    return (spec.__ref = direct(elt, (fn || err)(spec)));
+  /* Handle cases when spec is widget */
+  function widget(elt, spec) {
+    return (spec.__ref = direct(elt, ($[spec.widget] || err)(spec)));
+  }
+
+  /* Skip update if next spect is prev spec */
+  function update(elt, next, prev) {
+    return next === prev ? elt : element(elt, next, prev);
   }
 
   /* Render element node */
   function element(elt, next, prev) {
-    elt = reuse(elt, next);
-    prev = elt.__spec || 1;
+    elt = reuse(elt, next, prev);
     nodes(elt, arr(next.nodes), elt.childNodes);
-    attrs(elt, next.attrs, prev.attrs);
-    props(elt, next.props, prev.props);
-    events(elt, next.events, prev.events);
-    return (elt.__spec = next), (next.__ref = elt);
+    attrs(elt, next.attrs || 1, prev.attrs || 1);
+    props(elt, next.props || 1, prev.props || 1);
+    events(elt, next.events || 1, prev.events || 1);
+    elt.__spec = next;
+    return (next.__ref = elt);
   }
 
   /* Create or replace elt node */
-  function reuse(elt, next) {
-    var prev = elt ? elt.__spec || 1 : 1;
-    var name = elt ? (elt.nodeName || '').toLowerCase() : '';
+  function reuse(elt, next, prev) {
+    var name = (elt.nodeName || '').toLowerCase();
     var reuse = next.xmlns === prev.xmlns && next.name === name;
     return reuse ? elt : create(elt, next.name, next.xmlns);
   }
@@ -56,31 +58,26 @@
 
   /* Update child nodes */
   function nodes(elt, n, p, k) {
-    for (k = 0; k < n.length; k++) insert(elt, direct(p[k], n[k]), p[k]);
+    for (k = 0; k < n.length; k++) elt.insertBefore(direct(p[k], n[k]), p[k]);
     while (p[k]) elt.removeChild(p[k]);
-  }
-
-  /* Insert node if it's not equalt to previous */
-  function insert(elt, next, prev) {
-    if (next !== prev) elt.insertBefore(next, prev);
   }
 
   /* Update elt attrbutes */
   function attrs(elt, n, p, k) {
-    for (k in p) if (!has(n, k)) elt.removeAttribute(k);
-    for (k in n) if (!eq(n, p, k)) elt.setAttribute(k, n[k]);
+    for (k in p) if (n[k] == _) elt.removeAttribute(k);
+    for (k in n) if (n[k] !== p[k]) elt.setAttribute(k, n[k]);
   }
 
   /* Update elt properties */
   function props(elt, n, p, k) {
-    for (k in p) if (!has(n, k)) elt[k] = _;
-    for (k in n) if (!eq(n, p, k)) elt[k] = n[k];
+    for (k in p) if (n[k] == _) elt[k] = n[k];
+    for (k in n) if (n[k] !== p[k]) elt[k] = n[k];
   }
 
   /* Update elt event listeners */
   function events(elt, n, p, k) {
-    for (k in p) if (!has(n, p)) elt.removeEventListener(k, p[k]);
-    for (k in n) if (!eq(n, p, k)) elt.addEventListener(k, n[k]);
+    for (k in p) if (n[k] !== p[k]) elt.removeEventListener(k, p[k]);
+    for (k in n) if (n[k] !== p[k]) elt.addEventListener(k, n[k]);
   }
 
   /* Update or create a DOM TextNode with the value */
@@ -90,19 +87,9 @@
       : d.createTextNode(v);
   }
 
-  /* Validate that object o has not null property p */
-  function has(o, p) {
-    return o && Object.prototype.hasOwnProperty.call(o, p) && o[p] != _;
-  }
-
-  /* If a and b are both not null objects and have the same prop k */
-  function eq(a, b, k) {
-    return has(a, k) && has(b, k) && a[k] === b[k];
-  }
-
   /* Any to array */
   function arr(a) { return a ? (Array.isArray(a) ? a : [a]) : 1 }
 
-  /* Handle non existent widgets */
+  /* Handles non existent widgets */
   function err(model) { w.console.error('Widget not found', model) }
 })(this);
